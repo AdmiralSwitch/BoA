a9d4c41cea57f239b4fe69739f1d1c2f2eb5c7cb
Switch-Thomas <admiralswitch@gmail.com>
2015-04-03 17:35:17 -0700

    working on getting templateUrl to work in directives

--- app/assets/javascripts/app/directives.coffee
+++
@@ -1,5 +0,0 @@
-# resume tab directive
-angular.module 'boa.directives'
-  .directive 'resume', () ->
-    restrict: "E",
-    templateUrl: "resume.html"
--- app/assets/javascripts/app/directives.js
+++ app/assets/javascripts/app/directives.js
@@ -2,6 +2,13 @@
 app.directive("resume", function(){
   return {
     restrict: "E",
-    template: "./app/views/raffler/tabs/resume.html"
+    templateUrl: "tabs/resume.html"
+  };
+});
+
+app.directive("resume2", function(){
+  return {
+    restrict: "E",
+    template: "<div class='resume'><br><br><br><br><br><br><div><h1>Switch Thomas</h1>Inventor of the optigrab.<br>text in just the directive template.</div></div>"
   };
 });
